7eae1a264b35204ea6e68f7f51cc8f9841957365
partha <partha@divine-it.net>
2019-12-10 16:56:22 +0600

    update/view problem

--- charity/member/views.py
+++ charity/member/views.py
@@ -744,8 +744,9 @@ class UserCreateViewDash(generic.View):
 class UserUpdateViewDash(generic.View):
 
     def get(self, request, *args, **kwargs):
-        user_id = kwargs.get('pk')
-        # user_id = self.request.user
+        # user_id = kwargs.get('pk')
+        # user_id = self.request.user.id
+        user_id = request.user.id
         profile = get_object_or_404(UserProfileInfo, user_profile=user_id)
         user = get_object_or_404(User, id=profile.id)
         profile_info_form = UserProfileInfoForm(instance=profile)
@@ -753,11 +754,10 @@ class UserUpdateViewDash(generic.View):
         return render(request, 'user_update_form.html', {'profile_info_form': profile_info_form, 'user_info_form':user_info_form})
 
     def post(self, request, *args, **kwargs): 
-        user_id = kwargs.get('pk')
+        # user_id = kwargs.get('pk')
         
         # user_id = self.request.user.id
-       
-
+        user_id = request.user.id
         profile = get_object_or_404(UserProfileInfo, user_profile=user_id) 
         user = get_object_or_404(User, id=profile.id)  
 
